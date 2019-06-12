0bcf7f70306e75f4b59321e44dee33e98d223ac9
Alex Sirko <wisenss@gmail.com>
2019-06-12 09:55:23 +0300

    added configprovider

---
+++ src/test/java/com/ubs/common/ConfigProvider.java
@@ -0,0 +1,38 @@
+package com.ubs.common;
+
+
+import java.io.*;
+import java.util.Properties;
+
+public class ConfigProvider {
+
+    Properties configProperties = initProperties();
+
+    final String URI = getProperty("uri");
+
+    Properties initProperties()
+    {
+        if (configProperties == null)
+        {
+            configProperties = new Properties();
+            File propsFile = new File("C:\\Sources\\TestProject\\src\\test\\resources\\config.properties");
+            InputStream stream = null;
+            try {
+                stream = new FileInputStream(propsFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                configProperties.load(stream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return configProperties;
+    }
+
+    String getProperty(String propertyName)
+    {
+        return configProperties.getProperty(propertyName);
+    }
+}
